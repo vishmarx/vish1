f210b399434eef89567aaacbfe6c7e5e73434ee7
vishmarx <vishmarx@gmail.com>
2014-08-21 22:30:24 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vish1::Application.config.secret_token = '4f0dc9d1c7500d48d300e18605419b5344491062de23bf214161cd8baae55cf1c669e2fe529f03dc494c45522334930c714a4f6c30264a8d4834ef223895d200'
